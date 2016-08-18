6af21954736a59f79da93fecade03aa880530592
nabijaczleweli <nabijaczleweli@gmail.com>
2016-08-18 23:18:38 +0200

    Remove btreemap_append
    No longer needed
    
    Closes #7

--- src/util.rs
+++ src/util.rs
@@ -1,7 +1,6 @@
 //! Module containing various utility functions
 
 
-use std::collections::BTreeMap;
 use std::iter;
 
 
@@ -18,40 +17,6 @@ use std::iter;
 pub fn vec_merge<T>(mut lhs: Vec<T>, rhs: Vec<T>) -> Vec<T> {
     lhs.extend(rhs);
     lhs
-}
-
-// doc copied from BTreeMap::append()
-/// Moves all elements from `what` into `to`.
-///
-/// # Examples
-///
-/// ```
-/// use std::collections::BTreeMap;
-///
-/// let mut a = BTreeMap::new();
-/// a.insert(1, "a");
-/// a.insert(2, "b");
-/// a.insert(3, "c");
-///
-/// let mut b = BTreeMap::new();
-/// b.insert(3, "d");
-/// b.insert(4, "e");
-/// b.insert(5, "f");
-///
-/// checksums::util::btreemap_append(&mut a, b);
-///
-/// assert_eq!(a.len(), 5);
-///
-/// assert_eq!(a[&1], "a");
-/// assert_eq!(a[&2], "b");
-/// assert_eq!(a[&3], "d");
-/// assert_eq!(a[&4], "e");
-/// assert_eq!(a[&5], "f");
-/// ```
-pub fn btreemap_append<K: Ord, V>(to: &mut BTreeMap<K, V>, what: BTreeMap<K, V>) {
-    for (k, v) in what {
-        to.insert(k, v);
-    }
 }
 
 /// Create a string consisting of `n` repetitions of `what`.
