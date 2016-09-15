3b1d960bbfafc58f439cd8d80d191d8335b9d93f
nabijaczleweli <nabijaczleweli@gmail.com>
2016-09-15 10:16:38 +0200

    Simplify Algorithm::from_str()
    Lowercase and normalise separators before checking
    
    Thanks to Cicada and @milleniumbug for suggestions

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -72,29 +72,23 @@ impl FromStr for Algorithm {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "SHA1" | "SHA-1" | "sha1" => Ok(Algorithm::SHA1),
-            "SHA2256" | "SHA2-256" | "SHA-2-256" | "SHA2_256" | "SHA-2_256" | "SHA_2-256" | "SHA_2_256" | "sha2256" | "sha2-256" | "sha2_256" => {
-                Ok(Algorithm::SHA2256)
-            }
-            "SHA2" | "SHA-2" | "sha2" | "SHA2512" | "SHA2-512" | "SHA-2-512" | "SHA2_512" | "SHA-2_512" | "SHA_2-512" | "SHA_2_512" | "sha2512" |
-            "sha2-512" | "sha2_512" => Ok(Algorithm::SHA2512),
-            "SHA3256" | "SHA3-256" | "SHA-3-256" | "SHA3_256" | "SHA-3_256" | "SHA_3-256" | "SHA_3_256" | "sha3256" | "sha3-256" | "sha3_256" => {
-                Ok(Algorithm::SHA3256)
-            }
-            "SHA3" | "SHA-3" | "sha3" | "SHA3512" | "SHA3-512" | "SHA-3-512" | "SHA3_512" | "SHA-3_512" | "SHA_3-512" | "SHA_3_512" | "sha3512" |
-            "sha3-512" | "sha3_512" => Ok(Algorithm::SHA3512),
-            "BLAKE" | "blake" => Ok(Algorithm::BLAKE),
-            "BLAKE2" | "blake2" => Ok(Algorithm::BLAKE2),
-            "CRC64" | "crc64" => Ok(Algorithm::CRC64),
-            "CRC32" | "crc32" => Ok(Algorithm::CRC32),
-            "CRC16" | "crc16" => Ok(Algorithm::CRC16),
-            "CRC8" | "crc8" => Ok(Algorithm::CRC8),
-            "MD5" | "md5" => Ok(Algorithm::MD5),
-            "MD6128" | "MD6-128" | "MD6_128" | "md6128" | "md6-128" | "md6_128" => Ok(Algorithm::MD6128),
-            "MD6256" | "MD6-256" | "MD6_256" | "md6256" | "md6-256" | "md6_256" => Ok(Algorithm::MD6256),
-            "MD6512" | "MD6-512" | "MD6_512" | "md6512" | "md6-512" | "md6_512" => Ok(Algorithm::MD6512),
-            "XOR8" | "xor8" => Ok(Algorithm::XOR8),
+        match &s.replace("_", "-").to_lowercase()[..] {
+            "sha-1" | "sha1" => Ok(Algorithm::SHA1),
+            "sha2256" | "sha2-256" | "sha-2-256" => Ok(Algorithm::SHA2256),
+            "sha2" | "sha-2" | "sha2512" | "sha2-512" | "sha-2-512" => Ok(Algorithm::SHA2512),
+            "sha3256" | "sha3-256" | "sha-3-256" => Ok(Algorithm::SHA3256),
+            "sha3" | "sha-3" | "sha3512" | "sha3-512" | "sha-3-512" => Ok(Algorithm::SHA3512),
+            "blake" => Ok(Algorithm::BLAKE),
+            "blake2" => Ok(Algorithm::BLAKE2),
+            "crc64" => Ok(Algorithm::CRC64),
+            "crc32" => Ok(Algorithm::CRC32),
+            "crc16" => Ok(Algorithm::CRC16),
+            "crc8" => Ok(Algorithm::CRC8),
+            "md5" => Ok(Algorithm::MD5),
+            "md6128" | "md6-128" => Ok(Algorithm::MD6128),
+            "md6256" | "md6-256" => Ok(Algorithm::MD6256),
+            "md6512" | "md6-512" => Ok(Algorithm::MD6512),
+            "xor8" => Ok(Algorithm::XOR8),
             _ => Err(format!("\"{}\" is not a recognised compression algorithm", s)),
         }
     }
